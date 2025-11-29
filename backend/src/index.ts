9ed3d68d2ecb3dd855ca8993286f1ac7a4b70d7c
Anuj Maheshwari <anuj.m@sustainiam.com>
2025-11-29 13:53:41 +0530

    initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running in TypeScript!" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal server error",
+  });
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
